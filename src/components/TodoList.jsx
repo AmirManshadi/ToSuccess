7adaa804a2e801c61dddc82472a018497592c928
amirmanshadi <amirmanshadi.code@gmail.com>
2023-04-11 20:06:41 +0430

    created TodoList.jsx component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,23 @@
+import Button from "./Button"
+import TodoInput from "./TodoInput"
+import TodoItem from "./TodoItem"
+import btnTypes from "../assets/buttons"
+
 const TodoList = () => {
-	return <div className="todo-list">todo</div>
+	return (
+		<div className="todo-container">
+			<form className="todo-input">
+				<TodoInput>
+					<Button icon={btnTypes.add} />
+				</TodoInput>
+			</form>
+			<div className="todo-list">
+				<TodoItem icon={btnTypes.done} />
+				<TodoItem icon={btnTypes.done} />
+				<TodoItem icon={btnTypes.done} />
+			</div>
+		</div>
+	)
 }
 
 export default TodoList
